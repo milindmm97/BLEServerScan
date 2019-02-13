8c712ccedc6589be214699317997e20f46f33658
Milind M <milindmmanoj@outlook.com>
2019-02-13 17:13:02 +0530

    App and ESP32 Code added
    App modification to add notifications in progress

---
+++ AppCode/BluetoothLowEnergy/app/src/main/java/com/bignerdranch/android/bluetoothtestbed/MainActivity.java
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.bluetoothtestbed;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bignerdranch.android.bluetoothtestbed.client.ClientActivity;
+import com.bignerdranch.android.bluetoothtestbed.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
+        binding.launchClientButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+                ClientActivity.class)));
+    }
+}
